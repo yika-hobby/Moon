5cd1c7025f2c819198078fe24712d57992c84ed1
yika-hobby <yoshi_hiro2-gp2@yahoo.co.jp>
2025-04-15 22:01:05 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.cs
@@ -0,0 +1,16 @@
+namespace Moon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("AAA");
+        }
+    }
+}
